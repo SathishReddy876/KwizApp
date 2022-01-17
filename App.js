d091e4a69ec7ad11b90b6a7ec73a74317349120b
SathishReddy876 <sathishreddy876@gmail.com>
2022-01-17 14:05:45 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ import React from 'react';
+ import {StyleSheet,Text,View,} from 'react-native';
+import Home from './screens/home';
+import Quiz from './screens/quiz';
+import Result from './screens/result';
+ 
+  const App= () => {
+    
+   return (
+     <View style={styles.container}>
+       <Home />
+       <Quiz />
+       <Result />
+     </View>
+     
+   );
+ };
+ 
+ const styles = StyleSheet.create({
+   container: {
+     padding: 40,
+     paddingHorizontal: 16,
+   },
+   });
+ 
+ export default App;
+ 
