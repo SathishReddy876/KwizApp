72a9d1e9ccbad457917355c6f86b9c2d40e6333f
SathishReddy876 <sathishreddy876@gmail.com>
2022-01-17 14:55:03 -0500

    Implementing Title and Logo

--- screens/home.js
+++ screens/home.js
@@ -1,16 +1,38 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Image,StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import Title from '../components/title'
 
 const Home = () => {
     return (
         <View>
+            <Title />
+        <View style={styles.bannerContainer}>
+            <Image source={{
+                uri:'https://cdn.iconscout.com/icon/premium/png-64-thumb/quiz-2725865-2267376.png'
+            }}
+            style={styles.banner}
+            resizeMode='contain'
+            />
+        </View>
+        <TouchableOpacity>
             <Text>
-                This is Home
+                Start
             </Text>
+
+        </TouchableOpacity>
         </View>
     )
 }
 
-export default Home
+const styles = StyleSheet.create({
+    banner: {
+      height: 100,
+      width: 300,
+    },
+    bannerContainer:{
+        justifyContent:'center',
+        alignItems:'center'
+    }
+    });
 
-const styles = StyleSheet.create({})
+export default Home
